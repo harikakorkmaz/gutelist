a6c9a911a5d97d28318cc80fdc47264b7c5abb97
harikakorkmaz <harikanagato@gmail.com>
2020-12-13 04:43:20 +0000

    [edit] routing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,18 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+
+  def after_sign_in_path_for(resource)
+    tasks_root_path
+  end
+
+  def after_sign_out_path_for(resource)
+    new_user_session_path
+  end
 
   def edit
     @user = current_user
   end
-  
+
   def update
     @user = current_user
     if @user.update(user_params)
@@ -16,11 +25,11 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
-  
+
   def show
     @user = User.find(params[:id])
   end
-  
+
   private
     def user_params
       params.require(:user).permit(:name, :introduction, :image_id)
--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,24 @@
 Rails.application.routes.draw do
-  
-  resources :tasks, only: [:index, :new, :create, :edit, :update, :change, :complete_tasks] do
-    member do
-      delete "/" => "tasks#destroy"
-      patch "/" => "tasks#change", as: "change"
-    end
-    
-    collection do 
-      get "/complete" => "tasks#complete"
-    end
+
+  authenticated :user do
+    root :to => "tasks#index"
   end
-  resources :users, only: [:edit, :update, :index] do
-    member do
-      get "/" => "users#show"
-    end
-  end
-  
+
   root 'tasks#top'
 
   devise_for :users
+
+  resources :tasks, only: [:index, :new, :create, :edit, :update, :change, :complete_tasks] do
+    member do
+      delete "/" => "tasks#destroy"
+      patch "/complete" => "tasks#change", as: "change"
+    end
+
+    collection do
+      get "/complete" => "tasks#complete"
+    end
+  end
+
+  resources :users, only: [:edit, :update, :index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
