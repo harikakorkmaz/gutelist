3c972a2a353d9e3d791373d684dc2b41405bbaf1
harikakorkmaz <harikanagato@gmail.com>
2021-01-07 11:23:39 +0000

    [add] cloud natural language API

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_01_153835) do
+ActiveRecord::Schema.define(version: 2021_01_06_084155) do
 
   create_table "completed_tasks", force: :cascade do |t|
     t.integer "user_id"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2021_01_01_153835) do
     t.boolean "is_active", default: true, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal "score", precision: 5, scale: 3
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
